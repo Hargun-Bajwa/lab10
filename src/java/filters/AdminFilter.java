d80946f0fb1ac78befeb65bece91d38491f20f18
839217 <839217@SAIT227287IT.ACDM.DS.SAIT.CA>
2021-07-22 18:17:01 -0600

    Completed.. regular users cant access /admin page

--- src/java/filters/AdminFilter.java
+++ src/java/filters/AdminFilter.java
@@ -5,6 +5,7 @@
  */
 package filters;
 
+import dataaccess.UserDB;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.PrintWriter;
@@ -15,6 +16,10 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import models.User;
 
 /**
  *
@@ -30,12 +35,37 @@ public class AdminFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response,
             FilterChain chain)
             throws IOException, ServletException {
+        //getting request and response
+        HttpServletRequest httpRequest = (HttpServletRequest)request;
+        HttpServletResponse httpResponse = (HttpServletResponse)response;
+        HttpSession session = httpRequest.getSession();
         
+        //getting email to run further checks
+        String email = (String)session.getAttribute("email");
+        
+        
+        UserDB userDB = new UserDB();
+        
+        //getting the user object associated with that email.
+        User user = userDB.get(email);
+        
+       // no need to check here for null email because that is already checked in Authentication filter.( requirement of lab, but already completed)
+        
+        //checking that if it is not admin then do not let them go to /admin url
+        if(user.getRole().getRoleId() != 1){
+            httpResponse.sendRedirect("notes");
+            return;
         }
         
         
+        chain.doFilter(request, response); 
+        }
+        
+        
+    @Override
     public void destroy() {        
     }
+    @Override
     public void init(FilterConfig filterConfig) {        
        
         }
